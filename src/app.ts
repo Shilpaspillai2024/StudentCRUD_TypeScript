105ea95cdfa5011ee50024494728974e90bcc8e0
Shilpaspillai2024 <shilpaspillaitvla@gmail.com>
2024-09-02 14:38:14 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import studentRoutes from "./routes/StudentRoutes";
+
+
+const app=express();
+
+const PORT =process.env.PORT || 3000;
+
+
+// middleware
+
+app.use(express.json());
+
+
+// Middleware
+
+app.use('/api', studentRoutes); // Use the imported routes
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port:http://localhost: ${PORT}`);
+});
+
